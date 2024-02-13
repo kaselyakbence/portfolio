780ecf5d1b80fa48ea76289b662ffc24e08ead1d
kaselyakbence <kaselyakbence.dev@gmail.com>
2024-02-13 18:29:08 +0100

    Initial comit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import NavBar from "./components/navbar/Index"
+
+const App = () => {
+
+  return <>
+    <NavBar />
+    <div className="container">
+
+    </div>
+  </>
+
+
+}
+
+export default App
