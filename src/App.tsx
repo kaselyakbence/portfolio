d9bf03b3e40df7a6a1527cfb681b437322597626
kaselyakbence <kaselyakbence.dev@gmail.com>
2024-02-15 01:24:07 +0100

    README cleaned up

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 
+import { Route, Routes } from "react-router-dom"
 import NavBar from "./components/navbar/Index"
 
 const App = () => {
@@ -6,7 +7,13 @@ const App = () => {
   return <>
     <NavBar />
     <div className="container">
+      <Routes>
+        <Route path="/" element={<p>Home</p>} />
+        <Route path="/about" element={<p>About</p>} />
+        <Route path="/skills" element={<p>Skills</p>} />
+        <Route path="/projects" element={<p>Projects</p>} />
 
+      </Routes>
     </div>
   </>
 
