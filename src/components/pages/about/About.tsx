18177d68b9e2f9f2bdb4341e4102e38aeabc7f60
kaselyakbence <kaselyakbence.dev@gmail.com>
2024-08-06 20:21:18 +0200

    About new layout

--- src/components/pages/about/About.tsx
+++ src/components/pages/about/About.tsx
@@ -4,18 +4,22 @@ const About = () => {
   return (
     <div className="about-page">
       <div className="card">
-        <p className="about-info">Living in Berlin, Germany</p>
-        <p className="about-info">
-          I build web complex application with the help of modern frameworks and
-          test complex software solutions.
-        </p>
-        <ul>
-          <li>Personal goals</li>
-          <li>Professional experience</li>
-          <li>Academic experience</li>
-        </ul>
+        <div className="card-left">
+          <h1 className="about-header">Living in Berlin, Germany</h1>
+          <p className="about-info">
+            I’m a Software Developer currently living in Berlin. I specialize in
+            Web development with Typescript and React, but I also have
+            experience with other languages and tools like Python or Java.
+          </p>
+          <div className="line" />
+          <ul>
+            <li>Personal goals</li>
+            <li>Professional experience</li>
+            <li>Academic experience</li>
+          </ul>
+        </div>
+        <div className="card-right"></div>
       </div>
-      <div className="card"></div>
     </div>
   );
 };
