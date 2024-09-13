dfcfa34ad7253273069764078e61a0646d20ebac
kaselyakbence <kaselyakbence.dev@gmail.com>
2024-09-13 02:05:53 +0200

    Navbar bug fixed

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,13 +1,42 @@
+import { useEffect, useRef } from "react";
 import "./section.scss";
+import { NavBarState } from "./navbar/NavBar";
 
 interface SectionProps {
-  id: string;
+  id: keyof NavBarState;
   children: React.ReactNode;
+  editNavbarState: (key: keyof NavBarState, value: boolean) => void;
 }
 
-const Section = ({ id, children }: SectionProps) => {
+const Section = ({ id, children, editNavbarState }: SectionProps) => {
+  const targetRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        editNavbarState(id, entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.5,
+      }
+    );
+
+    if (targetRef.current) {
+      observer.observe(targetRef.current);
+    }
+
+    return () => {
+      if (targetRef.current) {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        observer.unobserve(targetRef.current);
+      }
+    };
+  }, [id, editNavbarState]);
+
   return (
-    <section id={id} className="app-section">
+    <section id={id} className="app-section" ref={targetRef}>
       {children}
     </section>
   );
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,8 +1,15 @@
 import "./navbar.scss";
-import { Link } from "react-scroll";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
+import NavbarItem from "./subcomponents/NavbarItem";
 
-const NavBar = () => {
+export interface NavBarState {
+  home: boolean;
+  about: boolean;
+  skills: boolean;
+  projects: boolean;
+}
+
+const NavBar = (navbarState: NavBarState) => {
   return (
     <div className="nav-bar">
       <div className="icons">
@@ -22,26 +29,12 @@ const NavBar = () => {
         </a>
       </div>
       <div className="links">
-        <Link to="home" className="nav-link" activeClass="active" smooth spy>
-          <p>Home</p>
-        </Link>
-        <Link to="about" className="nav-link" activeClass="active" smooth spy>
-          <p>About</p>
-        </Link>
-        <Link to="skills" className="nav-link" activeClass="active" smooth spy>
-          <p>Skills</p>
-        </Link>
-        <Link
-          to="projects"
-          className="nav-link"
-          activeClass="active"
-          smooth
-          spy
-        >
-          <p>Projects</p>
-        </Link>
+        {(Object.keys(navbarState) as (keyof NavBarState)[]).map((key) => (
+          <NavbarItem id={key} key={key} active={navbarState[key]} />
+        ))}
       </div>
     </div>
   );
 };
+
 export default NavBar;
