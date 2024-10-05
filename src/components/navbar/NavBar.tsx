440a766c8b65be7d3a09fa05e930802660608cec
kaselyakbence <kaselyakbence.dev@gmail.com>
2024-10-05 18:27:59 +0200

    First responsive changes

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,6 +1,7 @@
 import "./navbar.scss";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import NavbarItem from "./subcomponents/NavbarItem";
+import { useState } from "react";
 
 export interface NavBarState {
   home: boolean;
@@ -10,6 +11,8 @@ export interface NavBarState {
 }
 
 const NavBar = (navbarState: NavBarState) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="nav-bar">
       <div className="icons">
@@ -30,18 +33,36 @@ const NavBar = (navbarState: NavBarState) => {
       </div>
       <div className="links">
         <>
-          {/* <div className="ham-menu active">
+          <div
+            className={isOpen ? "ham-menu active" : "ham-menu"}
+            onClick={() => setIsOpen(!isOpen)}
+          >
             <span></span>
             <span></span>
             <span></span>
-          </div> */}
+          </div>
           {(Object.keys(navbarState) as (keyof NavBarState)[]).map((key) => (
-            <NavbarItem id={key} key={key} active={navbarState[key]} />
+            <NavbarItem
+              id={key}
+              key={key}
+              active={navbarState[key]}
+              mode="navbaritem"
+            />
           ))}
         </>
       </div>
 
-      {/* <div className="sidemenu active"></div> */}
+      <div className={isOpen ? "sidemenu active" : "sidemenu"}>
+        {(Object.keys(navbarState) as (keyof NavBarState)[]).map((key) => (
+          <NavbarItem
+            id={key}
+            key={key}
+            active={navbarState[key]}
+            mode="sidebaritem"
+            onClick={() => setIsOpen(false)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
