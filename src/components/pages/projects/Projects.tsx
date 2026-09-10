0adc4ca7ad723fe104dd72cb42e4c15997f0ff61
kaselyakbence <kaselyakbence.dev@gmail.com>
2026-09-10 22:28:57 +0200

    New project

--- src/components/pages/projects/Projects.tsx
+++ src/components/pages/projects/Projects.tsx
@@ -2,17 +2,17 @@ import "./projects.scss";
 import Project from "./subcomponents/Project";
 
 import portfolio from "../../../assets/projects/portfolio.png";
-// import adk from "../../../assets/projects/adk.png";
+import adk from "../../../assets/projects/adk-home.png";
 
 const Projects = () => {
   return (
     <div className="project-grid">
-      {/* <Project
+      <Project
         name="ADK Dormitory Page"
         githubLink="https://github.com/kaselyakbence/adk-washing"
-        demoLink="https://adk.kaselyakbence.com/"
+        demoLink="https://3-66-105-218.sslip.io/en"
         imgSrc={adk}
-      /> */}
+      />
       <Project
         name="Portfolio"
         githubLink="https://github.com/kaselyakbence/portfolio"
